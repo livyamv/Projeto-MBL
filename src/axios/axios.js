fdf4ba0b72b0910ac7a4f652fbb03fdd02e7e111
livyamv <liviareis2702@gmail.com>
2025-08-18 15:49:09 -0300

    login-liviaOK

---
+++ src/axios/axios.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+const api = axios.create({
+  baseURL: "http://10.89.240.68:3000/projeto_final",
+  headers: { accept: "application/json" },
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await SecureStore.getItemAsync("token"); // pega token de forma assíncrona
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.postCadastro = (usuario) => api.post("/usuario", usuario);
+api.postLogin = (usuario) => api.post("/login", usuario);
+
+export default api;
